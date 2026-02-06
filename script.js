53017b8615fb26718602459216a4e9c9cef0b877
Boussennadir <162161019+Boussennadir@users.noreply.github.com>
2026-02-06 19:08:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ title.forEach((title) => {
   title.addEventListener("click", () => {
     const answer = title.nextElementSibling;
     const icon = title.querySelector(".plus-icon");
+
     answer.hidden = !answer.hidden;
     if (answer.hidden) {
       icon.src = "./assets/images/icon-plus.svg";
